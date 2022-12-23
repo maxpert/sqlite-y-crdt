262ce2d51a93b39b32a4cbf242777b92545f2a70
Zohaib <zohaib.hassan@gmail.com>
2022-12-23 09:35:33 -0500

    Adding more tests

--- test-harness/test.js
+++ test-harness/test.js
@@ -1,4 +1,4 @@
-const sqlite = require('better-sqlite3')
+const sqlite = require('better-sqlite3');
 const Y = require('yjs');
 const assert = require('assert');
 
@@ -6,10 +6,28 @@ describe('YCRDT functions v1', () => {
     let db = sqlite(':memory:', {});
     beforeEach(() => {
         db = sqlite(':memory:', {});
-        db.loadExtension("../target/debug/libsqlite_y_crdt");
+        db.exec("CREATE TABLE dstore(k string PRIMARY KEY , v BLOB)")
+            .loadExtension("../target/debug/libsqlite_y_crdt");
     });
 
-    it('should be able to merge with empty doc', () => {
+    function insert_doc(k, doc) {
+        const data = Y.encodeStateAsUpdate(doc);
+        return db.prepare("INSERT OR REPLACE INTO dstore(k, v) VALUES (?, ?) RETURNING k, v").get(k, data);
+    }
+
+    function merge_doc(k, doc) {
+        const data = Y.encodeStateAsUpdate(doc);
+        return db.prepare("UPDATE dstore SET v = ydoc_merge_update(v, ?, 1) WHERE k = ?").run(data, k);
+    }
+
+    function get_doc(k) {
+        const {v} = db.prepare("SELECT v FROM dstore WHERE k = ?").get(k);
+        const doc = new Y.Doc();
+        Y.applyUpdate(doc, v);
+        return doc;
+    }
+
+    it('should be able to merge with empty doc using v1', () => {
         const doc = new Y.Doc();
         doc.getMap('map').set('test', 'Lorem Ipsum');
 
@@ -21,16 +39,8 @@ describe('YCRDT functions v1', () => {
         Y.applyUpdate(read_doc, row["yd"]);
         assert(read_doc.getMap('map').get('test') === doc.getMap('map').get('test'));
     });
-});
 
-describe('YCRDT functions v2', () => {
-    let db = sqlite(':memory:', {});
-    beforeEach(() => {
-        db = sqlite(':memory:', {});
-        db.loadExtension("../target/debug/libsqlite_y_crdt");
-    });
-
-    it('should be able to merge with empty doc', () => {
+    it('should be able to merge with empty doc using v2', () => {
         const doc = new Y.Doc();
         doc.getMap('map').set('test', 'Lorem Ipsum');
 
@@ -41,6 +51,21 @@ describe('YCRDT functions v2', () => {
         const read_doc = new Y.Doc();
         Y.applyUpdateV2(read_doc, row["yd"]);
         assert(read_doc.getMap('map').get('test') === doc.getMap('map').get('test'));
+    });
+
+    it('should merge stored value with document updates', () => {
+        let doc = new Y.Doc();
+        doc.getMap('map').set('test1', 'A');
+        insert_doc("key", doc);
+
+        doc = new Y.Doc();
+        doc.getMap('map').set('test2', 'B');
+        merge_doc("key", doc);
+
+        doc = get_doc("key");
+        const dict = doc.getMap('map').toJSON();
+        assert(dict.test1 === 'A');
+        assert(dict.test2 === 'B');
     });
 });
 
